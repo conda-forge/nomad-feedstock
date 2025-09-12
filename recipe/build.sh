caaffa826a1ec8e509251ee15f7b58d2b014bf21
Uwe Korn <uwelk@xhochy.com>
2025-09-12 13:55:12 +0000

    Fix aarch64 linkage

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,7 @@ export GO111MODULE=on
   # Set GOARCH based on build_platform to install native binaries
   export GOBIN="${BUILD_PREFIX}/bin"
   export CC="${CC_FOR_BUILD}"
+  export LDFLAGS="${LDFLAGS//$PREFIX/$BUILD_PREFIX}"
   case "$build_platform" in
     osx-64)
       export GOARCH=amd64
